0a6cae16af5fcaab848e5e4e70d10a4f5fe51664
BRO-BHATT22 <144465378+BRO-BHATT22@users.noreply.github.com>
2023-12-18 23:56:58 +0000

    added a bunch of images and a cool font. As it stands, I am done. But, live server is not working so I am going to go in early tomorrow to fix it. But, I will still turn it in here.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function shakeMagic8Ball() {
     if (question === null) {
         document.getElementById('response-text').innerText = 'Bro, you are not sneaky.. Ask a question'
         document.getElementById('response-text').classList = 'text-danger'
-        document.getElementById('response-image').src = '#BAD/IMAGE'
+        document.getElementById('response-image').src = 'SP000073-500x500.webp'
         return
     }
 
@@ -11,99 +11,49 @@ function shakeMagic8Ball() {
         alert('Please enter a valid question!')
         return
     }
-    const randomNumber = Math.floor(Math.random() * 18)
+    const randomNumber = Math.floor(Math.random() * 8)
     let answer, image, color
 
     switch (randomNumber) {
         case 0:
             answer = "Yes"
-            image = "Yes-image.jpg" //change please
+            image = "s-l1200.jpg" 
             color = "text-success"
             break
         case 1:
             answer = "It is decidedly so"
-            image = "Yes-image.jpg" //change please
+            image = "41tEphlx2hL._AC_.jpg" 
             color = "text-primary"
             break
         case 2:
             answer = "Reply hazy, please try again"
-            image = "Yes-image.jpg" //change please
+            image = "FaIyt53WQAEQYe-.png" //change please
             color = "text-muted"
             break
         case 3:
             answer = "Cannot predict now"
-            image = "Yes-image.jpg" //change please
+            image = "500_F_115106667_PIfPY9i1x5kZsljc2zKotT0WL1pJOJoe.jpg" //change please
             color = "text-primary"
             break
         case 4:
             answer = "Don't count on it mah boi"
-            image = "Yes-image.jpg" //change please
+            image = "1f95eb8f18e173d16b1ff3e633f68903.png" //change please
             color = "text-danger"
             break
         case 5:
             answer = "Yikes, the outlook ain't looking so hot"
-            image = "Yes-image.jpg" //change please
+            image = "41dEe8CqAHL._AC_.jpg" //change please
             color = "text-success"
             break
         case 6:
             answer = "I'm checking my sources now and they all say no"
-            image = "Yes-image.jpg" //change please
+            image = "082f12985721a25fef0cbf36bcb746ec (1).jpg" //change please
             color = "text-muted"
             break
         case 7:
             answer = "You're in luck, the sign point to yes"
-            image = "Yes-image.jpg" //change please
+            image = "0193674-0000000-0474762-primary.webp" //change please
             color = "text-success"
-            break
-            case 8:
-            answer = "Without a doubt"
-            image = "Yes-image.jpg" //change please
-            color = "text-warning"
-            break
-            case 9:
-            answer = "As I see it, yeah"
-            image = "Yes-image.jpg" //change please
-            color = "text-success"
-            break
-            case 10:
-            answer = "oof, I can't tell you now"
-            image = "Yes-image.jpg" //change please
-            color = "text-warning"
-            break
-            case 11:
-            answer = "Try asking again"
-            image = "Yes-image.jpg" //change please
-            color = "text-dark"
-            break
-            case 12:
-            answer = "I'm pretty doubtful, thats just me though"
-            image = "Yes-image.jpg" //change please
-            color = "text-info"
-            break
-            case 13:
-            answer = "Long rest, and try again"
-            image = "Yes-image.jpg" //change please
-            color = "text-warning"
-            break
-            case 14:
-            answer = "Trust me, you don't want to know"
-            image = "Yes-image.jpg" //change please
-            color = "text-primary"
-            break
-            case 15:
-            answer = "Meh"
-            image = "Yes-image.jpg" //change please
-            color = "text-secondary"
-            break
-            case 16:
-            answer = "Yessir"
-            image = "Yes-image.jpg" //change please
-            color = "text-success"
-            break
-            case 17:
-            answer = "nah bro"
-            image = "Yes-image.jpg" //change please
-            color = "text-danger"
             break
         default:
             break
