c747a6e64c2979eb1a7f0626e8b9299b2f81446d
BRO-BHATT <525sunaybhatt@frhsd.com>
2023-12-20 12:30:26 -0500

    Im done

--- script.js
+++ script.js
@@ -17,42 +17,42 @@ function shakeMagic8Ball() {
     switch (randomNumber) {
         case 0:
             answer = "Yes"
-            image = "s-l1200.jpg" 
+            image = "yes.png" 
             color = "text-success"
             break
         case 1:
             answer = "It is decidedly so"
-            image = "41tEphlx2hL._AC_.jpg" 
+            image = "i-guess-so-philip-j-fry.gif" 
             color = "text-primary"
             break
         case 2:
             answer = "Reply hazy, please try again"
-            image = "FaIyt53WQAEQYe-.png" //change please
+            image = "cog.jpg" //change please
             color = "text-muted"
             break
         case 3:
             answer = "Cannot predict now"
-            image = "500_F_115106667_PIfPY9i1x5kZsljc2zKotT0WL1pJOJoe.jpg" //change please
+            image = "idk.jpg" //change please
             color = "text-primary"
             break
         case 4:
             answer = "Don't count on it mah boi"
-            image = "1f95eb8f18e173d16b1ff3e633f68903.png" //change please
+            image = "count.png" //change please
             color = "text-danger"
             break
         case 5:
             answer = "Yikes, the outlook ain't looking so hot"
-            image = "41dEe8CqAHL._AC_.jpg" //change please
+            image = "yikes.png" //change please
             color = "text-success"
             break
         case 6:
             answer = "I'm checking my sources now and they all say no"
-            image = "082f12985721a25fef0cbf36bcb746ec (1).jpg" //change please
+            image = "source.jpg" //change please
             color = "text-muted"
             break
         case 7:
             answer = "You're in luck, the sign point to yes"
-            image = "0193674-0000000-0474762-primary.webp" //change please
+            image = "point.jpg" //change please
             color = "text-success"
             break
         default:
@@ -63,6 +63,6 @@ function shakeMagic8Ball() {
     const responseText = document.getElementById('response-text')
     responseText.innerText = answer
     responseText.classList = `display-4 ${color}`
-    document.getElementById('response-image').src = `images/${image}`
+    document.getElementById('response-image').src = `${image}`
 
 }
